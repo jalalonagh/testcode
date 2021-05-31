f7f6cbabc96a674d91fb1a632690fa9375b3264d
jalal <jalal@DESKTOP-MSA89D1>
2021-05-31 21:51:54 +0430

    sync

--- ManaDataTransferObject/User/UserDTOSearch.cs
+++ ManaDataTransferObject/User/UserDTOSearch.cs
@@ -14,13 +14,8 @@ namespace ManaDataTransferObject.User
         public int? Age { get; set; }
         public UserGenderType? Gender { get; set; }
         public int? SalcustSi { get; set; }
-        public string SalcustCu { get; set; }
-        public string SalcustTp { get; set; }
-        public string Avatar { get; set; }
         public DateTimeOffset? LastLoginDate { get; set; }
         public string NikName { get; set; }
-        public string ValidCode { get; set; }
-        public DateTime? ValidCodeExpired { get; set; }
         public bool? IsPerson { get; set; }
         public int? AccountingUserReferenceId { get; set; }
     }
