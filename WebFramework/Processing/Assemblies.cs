cecb50d4876e9e551881c6c812f8dad263333bb1
jalal.onagh <jalal.onagh@gmail.com>
2021-05-31 17:25:55 +0430

    sync

--- WebFramework/Processing/Assemblies.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-namespace SampleProject.Infrastructure.Processing
-{
-    internal static class Assemblies
-    {
-        public static readonly Assembly Application = typeof(BusinessLayout.IBL).Assembly;
-    }
-}
