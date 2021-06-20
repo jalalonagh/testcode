4461b747f877681876dc5450815cb889067f972d
jalal.onagh <jalal.onagh@gmail.com>
2021-06-20 10:10:56 +0430

    sync

--- WebFramework/Configuration/AutofacConfigurationExtensions.cs
+++ WebFramework/Configuration/AutofacConfigurationExtensions.cs
@@ -1,24 +1,17 @@
 ﻿using Autofac;
-using Autofac.Core;
 using Autofac.Core.Activators.Reflection;
 using Autofac.Extensions.DependencyInjection;
-using Autofac.Features.Variance;
 using BusinessLayout;
-using BusinessLayout.Configuration.Validation;
 using Common;
 using Data;
 using Data.Repositories;
 using Entities.Common;
-using FluentValidation;
-using ManaDapper;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Services.DataInitializer;
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -37,93 +30,6 @@ namespace WebFramework.Configuration
     internal static class Assemblies
     {
         public static readonly Assembly Application = typeof(IBL).Assembly;
-    }
-
-    public class MediatorModule : Autofac.Module
-    {
-        protected override void Load(ContainerBuilder builder)
-        {
-            builder.RegisterSource(new ScopedContravariantRegistrationSource(typeof(IRequestHandler<,>), typeof(INotificationHandler<>), typeof(IValidator<>)));
-            builder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly).AsImplementedInterfaces();
-            var mediatrOpenTypes = new[]
-            {
-            typeof(IRequestHandler<,>),
-            typeof(INotificationHandler<>),
-            typeof(IValidator<>)
-        };
-
-            foreach (var mediatrOpenType in mediatrOpenTypes)
-            {
-                builder
-                    .RegisterAssemblyTypes(Assemblies.Application, ThisAssembly)
-                    .AsClosedTypesOf(mediatrOpenType)
-                    .FindConstructorsWith(new AllConstructorFinder())
-                    .AsImplementedInterfaces();
-            }
-            builder.Register<ServiceFactory>(ctx =>
-            {
-                var c = ctx.Resolve<IComponentContext>();
-                return t => c.Resolve(t);
-            });
-            builder.RegisterGeneric(typeof(CommandValidationBehavior<,>)).As(typeof(IPipelineBehavior<,>));
-        }
-
-        private class ScopedContravariantRegistrationSource : IRegistrationSource
-        {
-            private readonly IRegistrationSource _source = new ContravariantRegistrationSource();
-            private readonly List<Type> _types = new List<Type>();
-            public ScopedContravariantRegistrationSource(params Type[] types)
-            {
-                if (types == null)
-                    throw new ArgumentNullException(nameof(types));
-                if (!types.All(x => x.IsGenericTypeDefinition))
-                    throw new ArgumentException("Supplied types should be generic type definitions");
-                _types.AddRange(types);
-            }
-            public IEnumerable<IComponentRegistration> RegistrationsFor(
-                Service service,
-                Func<Service, IEnumerable<ServiceRegistration>> registrationAccessor)
-            {
-                var components = _source.RegistrationsFor(service, registrationAccessor);
-                foreach (var c in components)
-                {
-                    var defs = c.Target.Services
-                        .OfType<TypedService>()
-                        .Select(x => x.ServiceType.GetGenericTypeDefinition());
-
-                    if (defs.Any(_types.Contains))
-                        yield return c;
-                }
-            }
-            public bool IsAdapterForIndividualComponents => _source.IsAdapterForIndividualComponents;
-        }
-    }
-
-    public class DataAccessModule : Autofac.Module
-    {
-        private readonly string _databaseConnectionString;
-        public DataAccessModule(string databaseConnectionString)
-        {
-            _databaseConnectionString = databaseConnectionString;
-        }
-        protected override void Load(ContainerBuilder builder)
-        {
-            builder.RegisterType<SqlConnectionFactory>()
-                .As<ISqlConnectionFactory>()
-                .WithParameter("connectionString", _databaseConnectionString)
-                .InstancePerLifetimeScope();
-            builder
-                .Register(c =>
-                {
-                    var dbContextOptionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-                    dbContextOptionsBuilder.UseSqlServer(_databaseConnectionString);
-
-                    return new ApplicationDbContext(dbContextOptionsBuilder.Options);
-                })
-                .AsSelf()
-                .As<DbContext>()
-                .InstancePerLifetimeScope();
-        }
     }
 
     public static class AutofacConfigurationExtensions
