1b361428aab8b003fea291f5be72d57c5c326e69
jalal.onagh <jalal.onagh@gmail.com>
2021-06-20 16:19:01 +0430

    sync

---
+++ ManaBaseEntity/Common/BaseSearchEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ManaBaseEntity.Common
+{
+    public class BaseSearchEntity : ISearchEntity
+    {
+        public int? Id { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public string CreatePersianTime { get; set; }
+        public DateTime? LastUpdateTime { get; set; }
+        public string LastUpdatePersianTime { get; set; }
+        public int? Order { get; set; }
+        public bool? IsActive { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+}
