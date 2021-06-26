b50cdd996016aca2b432f8fc4e80a2b8e14af3d4
jalal.onagh <jalal.onagh@gmail.com>
2021-06-26 17:14:08 +0430

    sync

---
+++ Data/DataContext.cs
@@ -0,0 +1,36 @@
+﻿using ManaBaseData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DataContext : ApplicationDbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public override DatabaseFacade Database => base.Database;
+        public override ChangeTracker ChangeTracker => base.ChangeTracker;
+        public override IModel Model => base.Model;
+        public override DbContextId ContextId => base.ContextId;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
