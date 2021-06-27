e432c5ddb7dc4044a1f3da302e3880eb7f160c74
jalal.onagh <jalal.onagh@gmail.com>
2021-06-27 09:41:44 +0430

    sync

--- ManaWebConfiguration/Configuration/AutofacConfigurationExtensions.cs
+++ ManaWebConfiguration/Configuration/AutofacConfigurationExtensions.cs
@@ -1,25 +1,12 @@
-﻿using Autofac;
-using Autofac.Core.Activators.Reflection;
-using Autofac.Extensions.DependencyInjection;
-using BusinessBaseConfig;
+﻿using Autofac.Core.Activators.Reflection;
 using Common;
-using Data;
-using Data.Repositories;
-using Entities.Common;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ProfileBusiness;
-using Services.DataInitializer;
-using SMSBusiness;
-using SMSConfirmationBusiness;
-using SMSRegexBusiness;
 using System;
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Reflection;
-using TransactionBusiness;
-using UserBusiness;
 
 namespace WebFramework.Configuration
 {
@@ -33,12 +20,12 @@ namespace WebFramework.Configuration
         }
     }
 
-    internal static class Assemblies
-    {
-        public static readonly Assembly Application = typeof(IBL).Assembly;
-        public static readonly Assembly[] Applications = new Assembly[] { typeof(IBL).Assembly, typeof(IPrB).Assembly,
-            typeof(ISmB).Assembly, typeof(ISCB).Assembly, typeof(ISRB).Assembly, typeof(ITrB).Assembly, typeof(IUsB).Assembly};
-    }
+    //internal static class Assemblies
+    //{
+    //    public static readonly Assembly Application = typeof(IBL).Assembly;
+    //    public static readonly Assembly[] Applications = new Assembly[] { typeof(IBL).Assembly, typeof(IPrB).Assembly,
+    //        typeof(ISmB).Assembly, typeof(ISCB).Assembly, typeof(ISRB).Assembly, typeof(ITrB).Assembly, typeof(IUsB).Assembly};
+    //}
 
     public static class AutofacConfigurationExtensions
     {
