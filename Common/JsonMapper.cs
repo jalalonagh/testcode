236fe7c1b0c7c8455c5cb618ebcbd13ff73a9441
jalal.onagh <jalal.onagh@gmail.com>
2021-08-31 13:59:28 +0430

    sef

---
+++ Common/JsonMapper.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Common
+{
+    public static class JsonMapper
+    {
+        public static T MapTo<T>(this object obj)
+        {
+            Newtonsoft.Json.Formatting fmt = Newtonsoft.Json.Formatting.None;
+            JsonSerializerSettings settings = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                PreserveReferencesHandling = PreserveReferencesHandling.None
+            };
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(obj, fmt, settings);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
