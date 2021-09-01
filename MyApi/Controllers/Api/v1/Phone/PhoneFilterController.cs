504279ab15b477b84ccfb46c483d904a77776a84
jalal.onagh <jalal.onagh@gmail.com>
2021-09-01 14:00:15 +0430

    sd

--- MyApi/Controllers/Api/v1/Phone/PhoneFilterController.cs
+++ MyApi/Controllers/Api/v1/Phone/PhoneFilterController.cs
@@ -9,12 +9,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace MyApi.Controllers.Api.v1
 {
     [ApiVersion("1")]
-    public class PhoneRangeController : GenericRangeController<Phone, PhoneValidator, PhoneSearch, PhoneVM, PhoneDTO, int>
+    public class PhoneFilterController : GenericFilterController<Phone, PhoneValidator, PhoneSearch, PhoneVM, PhoneDTO, int>
     {
-        private ICrudRange<Phone, PhoneValidator, PhoneSearch, PhoneDTO, int> crud;
+        private IFilter<Phone, PhoneValidator, PhoneSearch, PhoneDTO, int> crud;
         ResourceManagerSingleton resource;
 
-        public PhoneRangeController(ICrudRange<Phone, PhoneValidator, PhoneSearch, PhoneDTO, int> _crud) : base(_crud)
+        public PhoneFilterController(IFilter<Phone, PhoneValidator, PhoneSearch, PhoneDTO, int> _crud) : base(_crud)
         {
             resource = ResourceManagerSingleton.GetInstance();
             crud = _crud;
