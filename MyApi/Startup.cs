987a0705ddd45bce2878224e58f1be79939b7f9f
jalal.onagh <jalal.onagh@gmail.com>
2021-09-14 18:06:22 +0430

    dfgdfg

--- MyApi/Startup.cs
+++ MyApi/Startup.cs
@@ -1,15 +1,23 @@
 ﻿using Common;
 using ElmahCore.Mvc;
+using Entities.User;
+using Entities.User.Role;
+using ManaBaseData;
 using ManaDataTransferObjectValidator;
 using ManaEntitiesValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Converters;
 using SwaggerApi.V5;
 using System;
 using System.Globalization;
+using System.Text;
 using WebFramework.Configuration;
 using WebFramework.Configuration.AutofacConfigurations;
 using WebFramework.Configuration.JWTConfigurations;
@@ -40,12 +48,55 @@ namespace MyApi
             services.AddSession();
             services.Configure<SiteSettings>(Configuration.GetSection(nameof(SiteSettings)));
             services.AddDbContext(Configuration);
-            services.AddCustomIdentity(_siteSetting.IdentitySettings);
-            services.AddMinimalMvc();
+            services.AddIdentity<User, Role>(identityOptions =>
+            {
+                identityOptions.Password.RequireDigit = _siteSetting.IdentitySettings.PasswordRequireDigit;
+                identityOptions.Password.RequiredLength = _siteSetting.IdentitySettings.PasswordRequiredLength;
+                identityOptions.Password.RequireNonAlphanumeric = _siteSetting.IdentitySettings.PasswordRequireNonAlphanumic;
+                identityOptions.Password.RequireUppercase = _siteSetting.IdentitySettings.PasswordRequireUppercase;
+                identityOptions.Password.RequireLowercase = _siteSetting.IdentitySettings.PasswordRequireLowercase;
+                identityOptions.User.RequireUniqueEmail = _siteSetting.IdentitySettings.RequireUniqueEmail;
+            }).AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+            services.AddMvcCore(options =>
+            {
+                options.EnableEndpointRouting = false;
+            })
+                            .AddApiExplorer()
+                            .AddAuthorization(options =>
+                            {
+                                options.AddPolicy("Authorization", policyCorrectUser =>
+                                {
+                                    policyCorrectUser.Requirements.Add(new AuthorizationRequirement());
+                                });
+                            })
+                            .AddFormatterMappings()
+                            .AddDataAnnotations()
+                            .AddNewtonsoftJson(option =>
+                            {
+                                option.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                                option.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                                option.SerializerSettings.MaxDepth = 7;
+                                option.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.None;
+                            })
+                        .SetCompatibilityVersion(CompatibilityVersion.Latest);
             services.AddControllersWithViews();
             services.AddElmah(Configuration, _siteSetting);
-            new JWTServiceCollectionExtensions().AddJwtAuthentication(services, _siteSetting.JwtSettings);
-            services.AddCustomApiVersioning();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                var secretkey = Encoding.UTF8.GetBytes(_siteSetting.JwtSettings.SecretKey);
+                var encryptionkey = Encoding.UTF8.GetBytes(_siteSetting.JwtSettings.Encryptkey);
+                var validationParameters = _siteSetting.JwtSettings.GenerateValidationParameters(secretkey, encryptionkey);
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = validationParameters;
+                options.Events = _siteSetting.JwtSettings.GenerateJWTBearerEvent();
+            }); services.AddCustomApiVersioning();
             services.AddSwagger();
             services.AddSessionService();
             services.AddDTOValidationService(Configuration);
