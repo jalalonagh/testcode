e42338c2f61755ed7521e0e6c7e15c5d57cf98d4
jalal.onagh <jalal.onagh@gmail.com>
2021-09-15 16:20:50 +0430

    sync

--- MyApi/Startup.cs
+++ MyApi/Startup.cs
@@ -9,10 +9,9 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StartupConfiguration;
 using SwaggerApi.V5;
 using System;
 using System.Globalization;
@@ -44,44 +43,13 @@ namespace MyApi
             services.AddHttpClient();
             services.AddSession();
             services.Configure<SiteSettings>(Configuration.GetSection(nameof(SiteSettings)));
-            services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options
-                    .UseSqlServer(Configuration.GetConnectionString("SqlServer"));
-                options.EnableSensitiveDataLogging();
-            }, ServiceLifetime.Transient);
+            services.SetStartupAddDbContext<ApplicationDbContext, IConfiguration>(Configuration);
             services.AddIdentity<User, Role>(identityOptions =>
             {
-                identityOptions.Password.RequireDigit = _siteSetting.IdentitySettings.PasswordRequireDigit;
-                identityOptions.Password.RequiredLength = _siteSetting.IdentitySettings.PasswordRequiredLength;
-                identityOptions.Password.RequireNonAlphanumeric = _siteSetting.IdentitySettings.PasswordRequireNonAlphanumic;
-                identityOptions.Password.RequireUppercase = _siteSetting.IdentitySettings.PasswordRequireUppercase;
-                identityOptions.Password.RequireLowercase = _siteSetting.IdentitySettings.PasswordRequireLowercase;
-                identityOptions.User.RequireUniqueEmail = _siteSetting.IdentitySettings.RequireUniqueEmail;
+                identityOptions = identityOptions.SetStartupAddIdentityOptions(_siteSetting.IdentitySettings.MapTo<StartupConfiguration.Models.IdentitySettings>());
             }).AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultTokenProviders();
-            services.AddMvcCore(options =>
-            {
-                options.EnableEndpointRouting = false;
-            })
-                            .AddApiExplorer()
-                            .AddAuthorization(options =>
-                            {
-                                //options.AddPolicy("Authorization", policyCorrectUser =>
-                                //{
-                                //    policyCorrectUser.Requirements.Add(new AuthorizationRequirement());
-                                //});
-                            })
-                            .AddFormatterMappings()
-                            .AddDataAnnotations()
-                            .AddNewtonsoftJson(option =>
-                            {
-                                option.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                                option.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-                                option.SerializerSettings.MaxDepth = 7;
-                                option.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.None;
-                            })
-                        .SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.SetStartupAddMVCCoreBase();
             services.AddControllersWithViews();
             services.AddElmah(Configuration, _siteSetting);
             services.AddAuthentication(options =>
@@ -98,21 +66,13 @@ namespace MyApi
                 options.SaveToken = true;
                 options.TokenValidationParameters = validationParameters;
                 options.Events = _siteSetting.JwtSettings.GenerateJWTBearerEvent();
-            }); services.AddCustomApiVersioning();
+            });
+            services.AddCustomApiVersioning();
             services.AddSwagger();
             services.AddSessionService();
             services.AddDTOValidationService(Configuration);
             services.AddEntitiesValidationService(Configuration);
-            services.AddCors(options =>
-            {
-                options.AddPolicy(MyAllowSpecificOrigins,
-                builder =>
-                {
-                    builder.AllowAnyOrigin()
-                           .AllowAnyHeader()
-                           .AllowAnyMethod();
-                });
-            });
+            services.SetStartupAddCors(MyAllowSpecificOrigins);
             return services.BuildAutofacServiceProvider(Configuration);
         }
 
